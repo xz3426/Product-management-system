e0d2fe41b1048e14c914cf0bb840c5d722dda43c
xz3426 <xuanhuaz3426@gmail.com>
2023-09-20 19:41:22 -0400

    quick fix

--- src/Components/LandingPageComponents/NavBar.jsx
+++ src/Components/LandingPageComponents/NavBar.jsx
@@ -1,6 +1,6 @@
 import { Layout, Input, Badge, Avatar } from "antd";
 import { Link } from "react-router-dom";
-import { UserOutlined } from "@ant-design/icons";
+import { UserOutlined, ShoppingCartOutlined } from "@ant-design/icons";
 import React, { useState, useEffect } from "react";
 
 const { Header } = Layout;
@@ -8,33 +8,45 @@ const { Search } = Input;
 
 const NavBar_ = ({ isSignedIn }) => {
   return (
-    <Header
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between",
-        padding: "8px 64px 8px 64px",
-      }}
-    >
-      <h2>Management Chuwa</h2>
-      <Search
-        placeholder="Search"
-        allowClear
-        enterButton="Search"
-        size="middle"
-        style={{ width: "30%" }}
-        onSearch={() => {}}
-      />
-      <Badge>
-        <Avatar shape="square" icon={<UserOutlined />} />
-      </Badge>
+    <div className="nav-bar">
+      <Header
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          padding: "8px 64px 8px 64px",
+        }}
+      >
+        <a>
+          <h2>Management Chuwa</h2>
+        </a>
+        <Search
+          placeholder="Search"
+          allowClear
+          enterButton="Search"
+          size="middle"
+          style={{ width: "30%" }}
+          onSearch={() => {}}
+        />
 
-      {isSignedIn ? (
-        <Link to="/SignOut">SignOut</Link>
-      ) : (
-        <Link to="/SignIn">SignIn</Link>
-      )}
-    </Header>
+        <div>
+          <Badge>
+            <Avatar shape="square" icon={<UserOutlined />} />
+          </Badge>
+          {isSignedIn ? (
+            <Link to="/SignOut">SignOut</Link>
+          ) : (
+            <Link to="/SignIn">SignIn</Link>
+          )}
+        </div>
+        <Link>
+          <div to="/cart">
+            <ShoppingCartOutlined />
+            $0.00
+          </div>
+        </Link>
+      </Header>
+    </div>
   );
 };
 
