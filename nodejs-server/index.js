e9e502278ab6561bb1e83c309532ce3d69d470bc
xz3426 <xuanhuaz3426@gmail.com>
2023-09-26 19:37:40 -0400

    refactor to use routes

--- nodejs-server/index.js
+++ nodejs-server/index.js
@@ -2,13 +2,14 @@ const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname + "/.env") });
 const express = require("express");
 const app = express();
-const { signup, signin } = require("./src/handlers/auth");
 
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
-app.post("/api/auth/signup", signup);
-app.post("/api/auth/signin", signin);
+// Api-endpints
+
+// Authentication module url
+app.use("/api/auth", require("./src/routes/auth"));
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
