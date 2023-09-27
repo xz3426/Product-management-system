c868a376bab63135d7c047d0485f46a429e17e54
Koshie <57080973+wujumj@users.noreply.github.com>
2023-09-27 18:17:47 -0500

    merged the dev to authform

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {
 } from "./Components/LandingPageComponents";
 import { Layout } from "antd";
 import SignUp from 'pages/SignUp';
-import LogIn from 'pages/SignIn';
+import SignIn from 'pages/SignIn';
 import CreateProduct from "pages/CreateProduct";
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MyContent />} />
           <Route path="signup" element={<SignUp />} />
-          <Route path="login" element={<LogIn />} />
+          <Route path="signin" element={<SignIn />} />
           <Route path="createProduct" element={<CreateProduct />} />
         </Routes>
         <MyFooter></MyFooter>
